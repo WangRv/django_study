cd597a7d27adb9659ea392fc747bc5b73f66b650
WangRv <c269227455@gmail.com>
2020-07-17 19:55:36 +0800

    custom method added into the model.

--- home/models.py
+++ home/models.py
@@ -1,12 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
+# personalized models
+import datetime
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+    # addition method for adjudging recent question.
+    def was_published_recently(self):
+        # The publish date greater than or to equal last day that question is true.
+        return self.pub_date >= timezone.now()-datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.choice_text
