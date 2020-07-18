0734490898df380e3d51ac0f84f304cc1a21d5e7
WangRv <c269227455@gmail.com>
2020-07-18 21:29:36 +0800

    Home view has been completed.

--- home/views.py
+++ home/views.py
@@ -1,19 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import (render,
+                              get_object_or_404)
+from django.http import (HttpResponse,
+                         HttpResponseRedirect,
+                         Http404)
+from django.urls import reverse
+from django.views import generic
+# model importation
+from .models import (Question,
+                     Choice)
 
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello,index html.")
+class IndexView(generic.ListView):
+    template_name = "home/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "home/detail.html"
 
 
-def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}.")
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "home/results.html"
 
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "home/detail.html", {
+            "question": question,
+            "error_message": "You didn't select a choice."
+        })
+    else:
+        selected_choice.votes += 1  # vote this item
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("home:results", args=(question_id,) ))
