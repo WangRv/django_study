1a6b330e0d994cc40829617287eb95babdbe5169
WangRv <c269227455@gmail.com>
2020-07-19 17:15:27 +0800

    admin site page updated.

--- home/models.py
+++ home/models.py
@@ -15,6 +15,10 @@ class Question(models.Model):
         # The publish date greater than or to equal last day that question is true.
         return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+    was_published_recently.admin_order_field = "pub_date"  # use pub date attribute to sort.
+    was_published_recently.boolean = True
+    was_published_recently.short_description = "Recent published"
+
     def __str__(self):
         return self.question_text
 
